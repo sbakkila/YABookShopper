e1097e46c6db35102b94412128dd4571a49a13ee
Sam Bakkila <sbakkila@gmail.com>
2017-04-19 14:46:11 -0400

    created author route

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -8,6 +8,7 @@ export const Login = ({ login }) => (
     <input name="username" />
     <input name="password" type="password" />
     <input type="submit" value="Login" />
+    // make a button here to log-in with our Oauth providers
   </form>
 )
 
---
+++ server/author.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express()
+var Book = require('../db/models/book.js')
+var Publisher = require('../db/models/publisher.js')
+var Review = require('../db/models/review.js')
+var BooksGenres = require('../db/models/book.js')
+var Author = require('../db.models.author.js')
+
+app.get('/', function(req, res, next) {
+  // query string will look like ?author=authorLastName
+  if (req.query.author) {
+    Author.findOne({
+      where: {
+        lastName: req.query.author
+      }
+    })
+    .then(author => { author.getBooks() })
+    .then(myBooks => {
+      if (myBooks) {
+        res.send(myBooks)
+      } else {
+        res.sendStatus(404)
+      }
+    })
+    .catch(next)
+  } else {
+    res.sendStatus(404)
+  }
+})
