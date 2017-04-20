75010b2537cd4be88cd2fbdf528abefb74698229
Sam Bakkila <sbakkila@gmail.com>
2017-04-20 15:19:36 -0400

    responded to code review comments

--- server/books.js
+++ server/books.js
@@ -1,54 +1,35 @@
 var express = require('express')
-var app = express()
+var Router = express.Router()
 
 const db = require('APP/db')
 const Book = db.model('books')
 const Review = db.model('review')
 const Publisher = db.model('publisher')
 
-app.param('id', function(req, res, next, id) {
+Router.param('id', function(req, res, next, id) {
   // we want to add eager loading for reviews
   Book.findById(Number(id))
   .then(book => {
-      if (!book) {
-        // can refactor to use HttpError later
-        res.sendStatus(404)
-      } else {
-        req.book = book
-        next()
-      }
-    })
+    if (!book) {
+      // can refactor to use HttpError later
+      res.sendStatus(404)
+    } else {
+      req.book = book
+      next()
+    }
+  })
     .catch(next)
 })
 
-app.get('/', (req, res, next) => {
-  if (req.query.title) {
-    Book.findAll({
-      where: {
-        title: req.query.title
-      }
-    })
-    .then(books => res.send(books))
-    .catch(next)
-  } else if (req.query.publisher) {
-    Publisher.findAll({
-      where: {
-        name: req.query.publisher
-      },
-      include: [Book]
-    })
-      .then(booksByPublisher => {
-        res.send(booksByPublisher.books)
-      })
-      .catch(next)
-  } else {
-    Book.findAll({})
-    .then(books => res.send(books))
-    .catch(next)
-  }
+Router.get('/', (req, res, next) => {
+  Book.findAll({
+    where: req.query
+  })
+  .then(books => res.send(books))
+  .catch(next)
 })
 
-app.post('/', (req, res, next) => {
+Router.post('/', (req, res, next) => {
   // ToDo: find user, check if they are an admin
   Book.create(req.body)
     .then((book) => {
@@ -60,7 +41,7 @@ app.post('/', (req, res, next) => {
 
 // Return to this while writing Route Tests
 // get one book, and get all reviews for that book
-// app.get('/:id/reviews', (req, res, next) => {
+// Router.get('/:id/reviews', (req, res, next) => {
 //   Review.findAll({
 //     where: {
 //       bookId: req.book.id
@@ -74,7 +55,7 @@ app.post('/', (req, res, next) => {
 //   .catch(next)
 // })
 
-app.put('/:id', (req, res, next) => {
+Router.put('/:id', (req, res, next) => {
   // ToDo: find user, check if they are an admin
 
   req.book.update(req.body)
@@ -84,7 +65,7 @@ app.put('/:id', (req, res, next) => {
     .catch(next)
 })
 
-app.delete('/:id', (req, res, next) => {
+Router.delete('/:id', (req, res, next) => {
   // ToDo: find user, check if they are an admin
 
   // fix this later
@@ -99,14 +80,4 @@ app.delete('/:id', (req, res, next) => {
   .catch(next)
 })
 
-app.get('/genre/:genreId', (req, res, next) => {
-  Book.findAll({
-    where: {
-      genreId: req.params.genreId
-    }
-  })
-  .then(books => { res.send(books) })
-  .catch(next)
-})
-
-module.exports = app
+module.exports = Router
